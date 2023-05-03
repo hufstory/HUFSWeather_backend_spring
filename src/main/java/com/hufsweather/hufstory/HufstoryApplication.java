d8098c5552e3b67ff0e043c38abe2295c1944ecf
seungjaejeon <wjstmdwo2000@naver.com>
2023-05-03 20:30:40 +0900

    first commit

---
+++ src/main/java/com/hufsweather/hufstory/HufstoryApplication.java
@@ -0,0 +1,13 @@
+package com.hufsweather.hufstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HufstoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HufstoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hufsweather/hufstory/HufstoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hufsweather.hufstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HufstoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
